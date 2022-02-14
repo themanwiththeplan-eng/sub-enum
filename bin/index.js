9a5eba0083c97ebc610f177757bae5ecb8d8a0c7
themanwiththeplan-eng <dakotadunn5@gmail.com>
2022-02-14 04:23:25 -0800

    asn enumeration, fileprobe similar to httprobe, specport, probe single url

--- bin/index.js
+++ bin/index.js
@@ -4,14 +4,23 @@ const axios = require('axios').default;
 const fs = require('fs');
 const readline = require('readline');
 const lineReader = require('line-reader');
+const { error } = require('console');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 let args = yargs.argv;
 
 let path = args.p;
 let subdomain = args.d;
 let key = args.k;
+let fileprobe = args.fp;
+let defaultport = '443';
 let probe = args.probe;
-let port = args.port;
+let specport = args.sp;
+let asn = args.asn;
 
 if(path){
     lineReader.eachLine(path, function(line){
@@ -28,16 +37,82 @@ if(path){
         }
     })
     
-}else if(probe){
-    lineReader.eachLine(probe, function(line) {
-        console.log(`${line}`);
-        axios.get(line),{
+}else if(fileprobe){
+    lineReader.eachLine(fileprobe, function(line) {
+        let l = [];
+        l.push(line);
+        // console.log(l);
+        for(let i = 0; i < l.length; i++){
+            // console.log(l[i])
+            axios.get(l[i], {
 
-        }.then((response) =>{
-            console.log(response);
-        })
+            }).then((response) => {
+                // console.log(response.status);
+                if(response.status == 200){
+                    console.log(l[i])
+                }
+            }).catch(function(error){
+                if(error.response){
+                    console.log(error.response.status);
+                }else if(error.request){
+                    console.log(`Not a 200`);
+                }
+            })
+        }
+
+        
       });
     
+}else if(probe){
+      axios.get(probe + `:${defaultport}`, {
+
+      }).then((response) =>{
+        if(response.status == 200){
+            console.log(probe);
+        }
+      }).catch(function(error){
+          if(error.response){
+              console.log(error.response.status)
+          }else if(error.request){
+              console.log(`Not a 200`);
+          }
+      })
+}else if(specport){
+    rl.question(`URL:`, function(url){
+        rl.close();
+        let x = url + ':' + specport;
+        axios.get(x, {
+
+        }).then((response) => {
+            if(response.status == 200){
+                console.log(x);
+            }
+        }).catch(function(error){
+            if(error.response){
+                console.log(error.response.status)
+            }else if(error.request){
+                console.log(`Not a 200`);
+            }
+        })
+    })
+}else if(asn){
+    let asnurl = `https://api.hackertarget.com/aslookup?q=${asn}`
+    axios.get(asnurl, {
+
+    }).then((response) => {
+        console.log(response.data);
+    }).then(function(){
+        process.exit();
+    })
+    
 }else{
-    console.log(`Please use a flag`);
+    console.log(`The flags that you can use are: 
+    
+    --p : Use this with a file path to print the file line by line
+    --d : Use this for subdomain enumeration
+    --fp : Similar to httprobe, use this to scan a file for endpoints with status code of 200 on default port of 443
+    --probe : Use this to scan a single endpoint for status 200 using default port 443
+    --sp : Use this to scan single endpoint with any port that you will like, wizard setup... prompts for url
+    --asn: Use this to find autonomous system numbers`);
+    process.exit();
 }
